5a7d2f954b9e50d4589f78a03d73bb85d0f1c415
Ilya Sesyunin <ilia.sesiunin@samgau.com>
2024-03-27 10:06:37 +0500

    fix

--- tests/test_python_def.py
+++ tests/test_python_def.py
@@ -8,8 +8,6 @@ def test_dark_theme_by_time():
     current_time = time(hour=23)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
 
-    is_dark_theme = None
-
     if time(hour=6) <= current_time < time(hour=22):
         is_dark_theme = False
     else:
@@ -30,8 +28,6 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = None
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
-
-    is_dark_theme = None
 
     if dark_theme_enabled_by_user is True:
         is_dark_theme = True
